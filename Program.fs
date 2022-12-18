301a4db4f7aa39f473e738ee649d82ad74aa079c
xeno <xeno@lv427.space>
2022-12-18 21:50:32 +0100

    created a project for day 19

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System.IO
+open System 
+
+let (|Int|_|) (s:string) =
+    match Int64.TryParse s with
+    | true,value -> Some value
+    | false,_ -> None
+
+let file = File.ReadLines "/tmp/aoc/input" |> Seq.toList
+
+let parse (s:string) =
+    s
+    
+let input = file |> List.map parse
+
+input |> List.map (printfn "%A")
